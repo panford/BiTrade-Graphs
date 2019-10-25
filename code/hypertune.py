c45bc8d50e3f8a52310af0820fbbfb85e361d159
panford <panfordkobby@gmail.com>
2019-10-25 15:10:59 -0400

    Added hyperparameter tuner

---
+++ code/hypertune.py
@@ -0,0 +1,21 @@
+from sklearn.model_selection import GridSearchCV
+import pprint
+
+params = {'lr':[]
+         'weight_decay':[]
+         'K':[]
+         }
+def get_hparams():
+    
+    return hparams
+
+def set_hparams():
+    return
+
+def tuner(model,hparams):
+    
+    return best_params
+
+def print_params(best_params):
+    pprint(best_params)
+    return
